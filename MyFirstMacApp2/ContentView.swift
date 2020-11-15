900ed62d8d66c3360b9dcebdad36348eacde919f
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2020-11-15 22:06:11 +0900

    Initial Commit

---
+++ MyFirstMacApp2/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MyFirstMacApp2
+//
+//  Created by ucuc on 2020/11/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
