e72ad5fa2e321639edce01e0d11abc92e3b43a29
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2020-11-18 22:19:50 +0900

    wip

--- MyFirstMacApp2/ContentView.swift
+++ MyFirstMacApp2/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 // TODO
-// - 画面の下に表示したい
-// - listを表示したい
-// - ショートカットキーで呼びたい
-// - クリップボード監視したい
+// - [x] listを表示したい
+// - [ ] scrollさせる
+// - [ ] 画面の下に表示する
+// - [ ] ショートカットキーで呼ぶ
+// - [ ] クリップボード監視する
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        ScrollView(.horizontal, showsIndicators: true) {
+            HStack {
+                EmojiTile(text: "😀", color: .blue)
+                EmojiTile(text: "😉", color: .green)
+                EmojiTile(text: "😊", color: .purple)
+                EmojiTile(text: "😀", color: .blue)
+                EmojiTile(text: "😉", color: .green)
+                EmojiTile(text: "😊", color: .purple)
+                EmojiTile(text: "😀", color: .blue)
+                EmojiTile(text: "😉", color: .green)
+                EmojiTile(text: "😊", color: .purple)
+            }
+        }.frame(maxWidth: 1000, maxHeight: 1000)
+    }
+}
+
+struct EmojiTile: View {
+    var text: String
+    var color: Color
+    
+    var body: some View {
+        Text(text)
+            .font(.custom("Emoji", size: 50))
+            .padding(50)
+            .background(color)
+            .cornerRadius(10)
     }
 }
 
